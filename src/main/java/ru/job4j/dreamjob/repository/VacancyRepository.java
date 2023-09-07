02bc31cb9f188a7c888edfb4d2df6813a87e2eb1
ADIKIAA <zhekov.nick@yandex.ru>
2023-09-07 17:44:02 +0300

    4. Thymeleaf, Циклы. [#504841]

---
+++ src/main/java/ru/job4j/dreamjob/repository/VacancyRepository.java
@@ -0,0 +1,20 @@
+package ru.job4j.dreamjob.repository;
+
+import ru.job4j.dreamjob.model.Vacancy;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface VacancyRepository {
+
+    Vacancy save(Vacancy vacancy);
+
+    void delete(int id);
+
+    boolean update(Vacancy vacancy);
+
+    Optional<Vacancy> findBy(int id);
+
+    Collection<Vacancy> findAll();
+
+}
